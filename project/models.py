e36b16fa825da53d365a8636baaf8660f8818816
Frederik <r0802241@student.thomasmore.be>
2022-12-22 22:05:06 +0100

    Update models.py

--- project/models.py
+++ project/models.py
@@ -9,7 +9,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    hashed_password = Column(String, index=True)
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
